f6bbefbdf6a23f2051a61b02a6bf0f4d862b04e9
andrerochavit <142899637+andrerochavit@users.noreply.github.com>
2024-05-05 12:45:36 -0300

    Update Concessionaria.java

--- Concessionaria.java
+++ Concessionaria.java
@@ -1,5 +1,6 @@
 package spring;
 
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -8,180 +9,211 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Concessionaria extends JFrame {
-    private List<Veiculo> veiculos = new ArrayList<>();
+public class Clinica extends JFrame {
+
+    private List<Medico> medicos = new ArrayList<>();
+    private List<Paciente> pacientes = new ArrayList<>();
+    private List<Consulta> consultas = new ArrayList<>();
     private DefaultTableModel tableModel;
-    private JTable veiculosTable;
-    
-    public Concessionaria() {
-        setTitle("Sistema de Gerenciamento de Veículos");
-        setSize(600, 400);
+    private JTable medicosTable, pacientesTable, horariosTable;
+
+    public Clinica() {
+        setTitle("Clinica Fica Flinstones");
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        
+
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
-        
-        tableModel = new DefaultTableModel(new String[]{"Tipo", "Marca", "Modelo", "Ano", "Detalhes"}, 0);
-        veiculosTable = new JTable(tableModel);
-        JScrollPane scrollPane = new JScrollPane(veiculosTable);
-        panel.add(scrollPane, BorderLayout.CENTER);
-        
+
+        DefaultTableModel medicosTableModel = new DefaultTableModel(new String[]{"Nome", "Especialidade", "Crm", "Horarios Disponíveis"}, 0);
+        medicosTable = new JTable(medicosTableModel);
+        JScrollPane scrollPane = new JScrollPane(medicosTable);
+        panel.add(scrollPane, BorderLayout.NORTH);
+
+        DefaultTableModel pacientesTableModel = new DefaultTableModel(new String[]{"Nome", "CPF", "Endereco", "Contato", "Histórico médico"}, 0);
+        pacientesTable = new JTable(pacientesTableModel);
+        JScrollPane scrollPane1 = new JScrollPane(pacientesTable);
+        panel.add(scrollPane1, BorderLayout.CENTER);
+
+        DefaultTableModel horariosTableModel = new DefaultTableModel(new String[]{"Horário", "Médico", "Paciente"}, 0);
+        horariosTable = new JTable(horariosTableModel);
+        JScrollPane scrollPane11 = new JScrollPane(horariosTable);
+
+        JPanel southPanel = new JPanel(new BorderLayout());
+        southPanel.add(scrollPane11, BorderLayout.CENTER);
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
-        
-        JButton addButton = new JButton("Adicionar Veículo");
-        addButton.addActionListener(new ActionListener() {
+
+        JButton addMedicoButton = new JButton("Adicionar Médico");
+        addMedicoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                adicionarVeiculo();
+                adicionarMedico();
             }
         });
-        buttonPanel.add(addButton);
-        
-        JButton editButton = new JButton("Editar Veículo");
-        editButton.addActionListener(new ActionListener() {
+        buttonPanel.add(addMedicoButton);
+
+
+        JButton addPacienteButton = new JButton("Adicionar Paciente");
+        addPacienteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                editarVeiculo();
+                adicionarPaciente();
             }
         });
-        buttonPanel.add(editButton);
-        
-        JButton removeButton = new JButton("Remover Veículo");
-        removeButton.addActionListener(new ActionListener() {
+        buttonPanel.add(addPacienteButton);
+
+        JButton addmarcarConsultaButton = new JButton("Marcar Consulta");
+        addmarcarConsultaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                removerVeiculo();
+                marcarConsulta();
             }
         });
-        buttonPanel.add(removeButton);
-        
-        panel.add(buttonPanel, BorderLayout.SOUTH);
-        
+        buttonPanel.add(addmarcarConsultaButton);
+
+
+        JButton addRemoverPacienteButton = new JButton("Remover Paciente");
+        addRemoverPacienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removerPaciente();
+            }
+        });
+        buttonPanel.add(addRemoverPacienteButton);
+
+
+        JButton addRemoverMedicoButton = new JButton("Remover Médico");
+        addRemoverMedicoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removerMedico();
+            }
+        });
+        buttonPanel.add(addRemoverMedicoButton);
+
+
+        JButton addRemoverConsultaButton = new JButton("Remover consulta");
+        addRemoverConsultaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removerConsulta();
+            }
+        });
+        buttonPanel.add(addRemoverConsultaButton);
+
+        southPanel.add(buttonPanel, BorderLayout.SOUTH);
+        panel.add(southPanel, BorderLayout.SOUTH);
+
         add(panel);
         setVisible(true);
     }
-    
-    private void adicionarVeiculo() {
-        String tipoVeiculo = JOptionPane.showInputDialog(this, "Tipo de veículo (Carro/Moto/Caminhão):");
-        if (tipoVeiculo.equalsIgnoreCase("Carro")) {
-            adicionarCarro();
-        } else if (tipoVeiculo.equalsIgnoreCase("Moto")) {
-            adicionarMoto();
-        } else if (tipoVeiculo.equalsIgnoreCase("Caminhão")) {
-            adicionarCaminhao();
-        } else {
-            JOptionPane.showMessageDialog(this, "Tipo de veículo inválido.");
+
+    private void atualizarTabelaMedico() {
+        DefaultTableModel medicosTableModel = (DefaultTableModel) medicosTable.getModel();
+        medicosTableModel.setRowCount(0);
+        for (Medico medico : medicos) {
+            Object[] rowData = {medico.getNome(), medico.getEspecialidade(), medico.getCrm(), medico.getHorariosDisponiveis()};
+            medicosTableModel.addRow(rowData);
         }
     }
-    
-    private void adicionarCarro() {
-        String marca = JOptionPane.showInputDialog(this, "Marca:");
-        String modelo = JOptionPane.showInputDialog(this, "Modelo:");
-        int ano = Integer.parseInt(JOptionPane.showInputDialog(this, "Ano:"));
-        String combustivel = JOptionPane.showInputDialog(this, "Tipo de combustível:");
-        Carro carro = new Carro(marca, modelo, ano, combustivel);
-        veiculos.add(carro);
-        atualizarTabela();
+
+    private void atualizarTabelaPaciente() {
+        DefaultTableModel pacientesTableModel = (DefaultTableModel) pacientesTable.getModel();
+        pacientesTableModel.setRowCount(0);
+        for (Paciente paciente : pacientes) {
+            Object[] rowData = {paciente.getNome(), paciente.getCpf(), paciente.getEndereco(), paciente.getContato().concat(paciente.getHistoricoMedico())};
+            pacientesTableModel.addRow(rowData);
+        }
     }
-    
-    private void adicionarMoto() {
-        String marca = JOptionPane.showInputDialog(this, "Marca:");
-        String modelo = JOptionPane.showInputDialog(this, "Modelo:");
-        int ano = Integer.parseInt(JOptionPane.showInputDialog(this, "Ano:"));
-        int cilindrada = Integer.parseInt(JOptionPane.showInputDialog(this, "Cilindrada:"));
-        Moto moto = new Moto(marca, modelo, ano, cilindrada);
-        veiculos.add(moto);
-        atualizarTabela();
+
+    private void atualizarTabelaHorarios() {
+        DefaultTableModel horariosTableModel = (DefaultTableModel) horariosTable.getModel();
+        horariosTableModel.setRowCount(0);
+        for (Consulta consulta : consultas) {
+            Object[] rowData = {consulta.getHorario(), consulta.getMedico().getNome(), consulta.getPaciente().getNome()};
+            horariosTableModel.addRow(rowData);
+        }
     }
-    
-    private void adicionarCaminhao() {
-        String marca = JOptionPane.showInputDialog(this, "Marca:");
-        String modelo = JOptionPane.showInputDialog(this, "Modelo:");
-        int ano = Integer.parseInt(JOptionPane.showInputDialog(this, "Ano:"));
-        double capacidadeCarga = Double.parseDouble(JOptionPane.showInputDialog(this, "Capacidade de carga:"));
-        Caminhao caminhao = new Caminhao(marca, modelo, ano, capacidadeCarga);
-        veiculos.add(caminhao);
-        atualizarTabela();
-    }
-    
-    private void editarVeiculo() {
-        int selectedRow = veiculosTable.getSelectedRow();
+
+    private void removerConsulta() {
+        int selectedRow = horariosTable.getSelectedRow();
         if (selectedRow == -1) {
-            JOptionPane.showMessageDialog(this, "Selecione um veículo para editar.");
+            JOptionPane.showMessageDialog(this, "Selecione uma consulta para remover.");
             return;
         }
-        Veiculo veiculoSelecionado = veiculos.get(selectedRow);
-        if (veiculoSelecionado instanceof Carro) {
-            editarCarro((Carro) veiculoSelecionado);
-        } else if (veiculoSelecionado instanceof Moto) {
-            editarMoto((Moto) veiculoSelecionado);
-        } else if (veiculoSelecionado instanceof Caminhao) {
-            editarCaminhao((Caminhao) veiculoSelecionado);
-        }
+        consultas.remove(selectedRow);
+        atualizarTabelaHorarios();
     }
-    
-    private void editarCarro(Carro carro) {
-        String marca = JOptionPane.showInputDialog(this, "Nova marca:", carro.getMarca());
-        String modelo = JOptionPane.showInputDialog(this, "Novo modelo:", carro.getModelo());
-        int ano = Integer.parseInt(JOptionPane.showInputDialog(this, "Novo ano:", carro.getAno()));
-        String combustivel = JOptionPane.showInputDialog(this, "Novo tipo de combustível:", carro.getCombustivel());
-        carro.setMarca(marca);
-        carro.setModelo(modelo);
-        carro.setAno(ano);
-        carro.setCombustivel(combustivel);
-        atualizarTabela();
-    }
-    
-    private void editarMoto(Moto moto) {
-        String marca = JOptionPane.showInputDialog(this, "Nova marca:", moto.getMarca());
-        String modelo = JOptionPane.showInputDialog(this, "Novo modelo:", moto.getModelo());
-        int ano = Integer.parseInt(JOptionPane.showInputDialog(this, "Novo ano:", moto.getAno()));
-        int cilindrada = Integer.parseInt(JOptionPane.showInputDialog(this, "Nova cilindrada:", moto.getCilindrada()));
-        moto.setMarca(marca);
-        moto.setModelo(modelo);
-        moto.setAno(ano);
-        moto.setCilindrada(cilindrada);
-        atualizarTabela();
-    }
-    
-    private void editarCaminhao(Caminhao caminhao) {
-        String marca = JOptionPane.showInputDialog(this, "Nova marca:", caminhao.getMarca());
-        String modelo = JOptionPane.showInputDialog(this, "Novo modelo:", caminhao.getModelo());
-        int ano = Integer.parseInt(JOptionPane.showInputDialog(this, "Novo ano:", caminhao.getAno()));
-        double capacidadeCarga = Double.parseDouble(JOptionPane.showInputDialog(this, "Nova capacidade de carga:", caminhao.getCapacidadeCarga()));
-        caminhao.setMarca(marca);
-        caminhao.setModelo(modelo);
-        caminhao.setAno(ano);
-        caminhao.setCapacidadeCarga(capacidadeCarga);
-        atualizarTabela();
-    }
-    
-    private void removerVeiculo() {
-        int selectedRow = veiculosTable.getSelectedRow();
+
+    protected void removerMedico() {
+        int selectedRow = medicosTable.getSelectedRow();
         if (selectedRow == -1) {
             JOptionPane.showMessageDialog(this, "Selecione um veículo para remover.");
             return;
         }
-        veiculos.remove(selectedRow);
-        atualizarTabela();
+        medicos.remove(selectedRow);
+        atualizarTabelaMedico();
     }
-    
-    private void atualizarTabela() {
-        tableModel.setRowCount(0);
-        for (Veiculo veiculo : veiculos) {
-            String tipo = veiculo instanceof Carro ? "Carro" : (veiculo instanceof Moto ? "Moto" : "Caminhão");
-            Object[] rowData = {tipo, veiculo.getMarca(), veiculo.getModelo(), veiculo.getAno(), veiculo.toString()};
-            tableModel.addRow(rowData);
+
+    protected void removerPaciente() {
+        int selectedRow = pacientesTable.getSelectedRow();
+        if (selectedRow == -1) {
+            JOptionPane.showMessageDialog(this, "Selecione um veículo para remover.");
+            return;
+        }
+        pacientes.remove(selectedRow);
+        atualizarTabelaPaciente();
+    }
+
+
+    private void marcarConsulta() {
+        int selectedMedicoRow = medicosTable.getSelectedRow();
+        int selectedPacienteRow = pacientesTable.getSelectedRow();
+        if (selectedMedicoRow == -1 || selectedPacienteRow == -1) {
+            JOptionPane.showMessageDialog(this, "Selecione um médico e um paciente para marcar a consulta.");
+            return;
+        }
+        Medico medico = medicos.get(selectedMedicoRow);
+        Paciente paciente = pacientes.get(selectedPacienteRow);
+        String horariosDisponiveis = medico.getHorariosDisponiveis();
+        String horarioEscolhido = (String) JOptionPane.showInputDialog(this, "Escolha um horário para a consulta:",
+                "Horários Disponíveis", JOptionPane.PLAIN_MESSAGE, null, horariosDisponiveis.split(","), horariosDisponiveis.split(",")[0]);
+        if (horarioEscolhido != null) {
+            Consulta consulta = new Consulta(medico, paciente, horarioEscolhido);
+            consultas.add(consulta);
+            atualizarTabelaHorarios();
         }
     }
-    
+
+    protected void adicionarPaciente() {
+        String nome = JOptionPane.showInputDialog(this, "Nome:");
+        String cpf = JOptionPane.showInputDialog(this, "Cpf:");
+        String endereco = JOptionPane.showInputDialog(this, "Endereco:");
+        String contato = JOptionPane.showInputDialog(this, "Contato");
+        String historicoMedico = JOptionPane.showInputDialog(this, "Historico Medico:");
+        Paciente paciente = new Paciente(nome, cpf, endereco, contato, historicoMedico);
+        pacientes.add(paciente);
+        atualizarTabelaPaciente();
+    }
+
+    protected void adicionarMedico() {
+        String nome = JOptionPane.showInputDialog(this, "Nome:");
+        String especialidade = JOptionPane.showInputDialog(this, "Especialidade:");
+        String crm = JOptionPane.showInputDialog(this, "Crm:");
+        String horariosDisponiveis = JOptionPane.showInputDialog(this, "Contato");
+        Medico medico = new Medico(nome, especialidade, crm, horariosDisponiveis);
+        medicos.add(medico);
+        atualizarTabelaMedico();
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Concessionaria();
+                new Clinica();
             }
         });
     }
